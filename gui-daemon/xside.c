13ef075635407765f426c6aaa5451d646dfbbdcd
Chris Sauer <mr-sour@users.noreply.github.com>
2021-05-03 15:57:57 -0400

    CSD experiment

--- gui-daemon/xside.c
+++ gui-daemon/xside.c
@@ -60,6 +60,8 @@
 #include "png.h"
 #include "trayicon.h"
 #include "shm-args.h"
+#include <gtk/gtk.h>
+#include <gtk/gtkx.h>
 
 /* Supported protocol version */
 
@@ -109,7 +111,7 @@ static void inter_appviewer_lock(Ghandles *g, int mode);
 static void release_mapped_mfns(Ghandles * g, struct windowdata *vm_window);
 static void print_backtrace(void);
 static void parse_cmdline_prop(Ghandles *g);
-
+static void x11_win_reparent_to_gtk_csd(void); //static void x11_win_reparent_to_gtk_csd(Window *w);
 static void show_message(Ghandles *g, const char *prefix, const char *msg,
 			 gint timeout)
 {
@@ -421,6 +423,8 @@ static Window mkwindow(Ghandles * g, struct windowdata *vm_window)
                     g->extra_props[i].nelements);
         }
     }
+    
+    x11_win_reparent_to_gtk_csd(); //&child_win
 
     if (vm_window->remote_winid == FULLSCREEN_WINDOW_ID) {
         /* whole screen window */
@@ -3751,6 +3755,65 @@ void restart_guid() {
     perror("execv");
 }
 
+
+static void x11_win_reparent_to_gtk_csd(void)
+{
+
+
+    /* Create window Objects*/
+    GtkWidget *win  = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *sock = gtk_socket_new();
+    GtkWidget *header = gtk_header_bar_new ();
+    GdkDisplay *display = gdk_display_get_default();
+    GdkScreen *screen = gdk_display_get_default_screen (display);
+    GtkCssProvider *provider = gtk_css_provider_new ();
+    
+    //Add provider to style headerbar priority is 65535 which means it can't be overriden 
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER(provider), 65535);
+    //Simple little decoration to add to the window
+    gtk_css_provider_load_from_data (
+     provider, "decoration { border: 3px solid green; background: gray; } .titlebar { background: green ; color:white; }            .titlebar:backdrop  {background: green; color:white;}               window.ssd headerbar.titlebar { border: 5px; box-shadow: none;              background-image: linear-gradient(to bottom, shade(blue, 1.05),             shade(blue, 1.00)); }", -1, NULL);
+    //I like windows that close
+    gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (header), TRUE);
+    //subtitles? this show is already in my language
+    gtk_header_bar_set_has_subtitle (GTK_HEADER_BAR (header), FALSE);
+    //this would get set to "VMNAME - APPNAME" in the real app
+    gtk_header_bar_set_title (GTK_HEADER_BAR (header), "x11 window embedded in a window with CSD");
+    //swap default titlebar for headerbar to set CSD
+    gtk_window_set_titlebar (GTK_WINDOW (win), header);
+    
+    //connect sockets
+    //this is if the underlying window has been delete "ie: cancel dialog"
+    g_signal_connect(sock, "plug-removed", gtk_main_quit, NULL);
+    //this is for when you press the little X or ctrl+c out of program
+    g_signal_connect(win,  "delete-event", gtk_main_quit, NULL);
+
+    //Set default start size a little bigger.IDK might need todo some weirdness in the real but this is fine for demo 
+    gtk_widget_set_size_request(sock, 400, 200);
+    //why did add this?
+    //gtk_box_pack_start(GTK_BOX(vbox), sock, TRUE,  TRUE, 0);
+
+    //gotta add my wonderful nonexistant widgets or the sockets dont work
+    gtk_container_add(GTK_CONTAINER(win), sock);
+    
+    //realize gtk window (required before embedding another window)
+
+    //gtk_widget_show(sock);
+    //gtk_widget_realize( sock );
+    
+    gtk_widget_show_all(win);
+
+    // embed x11 window into a GtkSocket
+    
+    //gtk_socket_add_id(GTK_SOCKET(sock),*w);
+
+    //Call main cause it was in the example
+    //gtk_main();
+    //gtk_main_iteration();
+
+}
+
+
 int main(int argc, char **argv)
 {
     int xfd;
@@ -3774,6 +3837,9 @@ int main(int argc, char **argv)
     /* parse cmdline, possibly overriding values from config */
     parse_cmdline(&ghandles, argc, argv);
     get_boot_lock(ghandles.domid);
+    /* TODO dont init gtk if non csd window manager */    
+    gtk_init(NULL, NULL);
+
 
     if (!ghandles.nofork) {
         // daemonize...
@@ -3955,6 +4021,8 @@ int main(int argc, char **argv)
     send_xconf(&ghandles);
 
     for (;;) {
+        //non blocking gtk_main TODO skip if not in csd WM
+        gtk_main_iteration_do (FALSE);        
         int select_fds[2] = { xfd };
         fd_set retset;
         int busy;
